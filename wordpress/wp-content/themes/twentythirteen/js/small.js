a67f4f5505f0b54d05bfdbbb7dd080acfa79ed20
Johan Soulet <johan.soulet@gmail.com>
2013-05-23 13:25:58 +0200

    Add previously integrated files + header.php

---
+++ wordpress/wp-content/themes/twentythirteen/js/small.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    var mobile = document.createElement('div');
+    mobile.className = 'nav-mobile svg';
+    document.querySelector('.nav').appendChild(mobile);
+});
