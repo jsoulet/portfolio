15a495167408240c6d9c6557cd0d9b417839bee2
Johan Soulet <johan.soulet@gmail.com>
2013-05-24 16:09:28 +0200

    Make page.php

---
+++ wordpress/wp-content/themes/nineteenninety/page.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+<!-- /header.php -->
+<!-- content -->
+<section class="container_12 content">
+
+  <section class="grid_8 alpha omega main">
+    <?php if(have_posts()) : ?>
+      <?php while(have_posts()) : the_post(); ?>
+        <article>
+          <h2 id="single_title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+          
+          <?php if(has_excerpt()){ ?>
+            <div id="single_excerpt"><?php the_excerpt() ?></div>
+          <?php } ?>
+          <div id="single_content"><?php the_content(); ?></div>
+
+        <article>
+      <?php endwhile; ?>
+    <?php endif; ?>
+  </section>
+  <!-- /content -->
+  <?php get_sidebar(); ?>
+
+</section>
+<!-- footer -->
+<?php get_footer(); ?>
+<!-- /footer -->
+</body>
+</html>
