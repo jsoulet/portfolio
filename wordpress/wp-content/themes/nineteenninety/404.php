ca4825c9540aa35c7c49ff2640fefcbee35ead3b
Johan Soulet <johan.soulet@gmail.com>
2013-05-25 15:06:26 +0200

    404 page

---
+++ wordpress/wp-content/themes/nineteenninety/404.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+<!-- /header.php -->
+<!-- content -->
+<section class="container_12 content">
+
+  <section class="container_8 grid_8 alpha omega main">
+    <h2>404 : That page doesn't exist.</h2>
+    <p><br/>The requested URL was not found on this server.</p>
+    <p>The link you followed might be incorrect or the page doesn't exist anymore</p>
+    <p><br/>Go back to <a href="<?php bloginfo('url'); ?>">home</a> or <a href="contact">contact me</a></p>
+  </section>
+  <!-- /content -->
+  <?php get_sidebar(); ?>
+
+</section>
+<!-- footer -->
+<?php get_footer(); ?>
+<!-- /footer -->
+</body>
+</html>
