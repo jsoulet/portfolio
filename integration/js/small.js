a9b5f46ef2107646789c5447da8831e019d858db
Johan Soulet <johan.soulet@gmail.com>
2013-05-27 18:49:17 +0200

    Use Bootstrap

--- integration/js/small.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    var mobile = document.createElement('div');
-    mobile.className = 'nav-mobile svg';
-    document.querySelector('.nav').appendChild(mobile);
-});
